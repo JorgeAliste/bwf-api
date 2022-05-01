e15129e49843ec5e0eb4fe03dfeae4340d071ccb
jorgealiste <j.alistea@gmail.com>
2022-04-30 20:18:19 -0400

    Custom auth token class (post method)

--- bwfapi/urls.py
+++ bwfapi/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include, re_path
 
 from bwfapi import views
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register('groups', views.GroupViewSet)
 router.register('events', views.EventViewSet)
 
 urlpatterns = [
-    re_path(r'^', include(router.urls))
+    re_path(r'^', include(router.urls)),
+    path('authenticate/', views.CustomObtainAuthToken.as_view()),
 ]
--- bwfapi/views.py
+++ bwfapi/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 from .models import Group, Event
-from .serializers import GroupSerializer, EventSerializer, GroupFullSerializer
+from .serializers import GroupSerializer, EventSerializer, GroupFullSerializer, UserSerializer
 
 
 class GroupViewSet(viewsets.ModelViewSet):
@@ -19,3 +21,13 @@ class GroupViewSet(viewsets.ModelViewSet):
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+
+class CustomObtainAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        response = super(CustomObtainAuthToken, self).post(request, *args, **kwargs)  # TODO: arguments of super
+        token = Token.objects.get(key=response.data['token'])
+        user = User.objects.get(id=token.user_id)
+        user_serializer = UserSerializer(user)
+        return Response({'token': token.key, 'user': user_serializer.data})
